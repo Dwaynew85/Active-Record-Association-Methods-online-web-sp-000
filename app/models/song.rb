ae6156da3fe35cc05fa1296d17a22e2d3f89a7dc
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-17 07:14:29 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,6 +11,6 @@ class Song < ActiveRecord::Base
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
     Artist.find_by(name: "Drake") ? drake = Artist.find_by(name: "Drake") : drake = Artist.new(name: "Drake")
-    drake.songs << self
+    self.artist = drake
   end
 end
